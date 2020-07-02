d23760b586f2f927e7f6a857299d6d9ec8f0ce0d
Rahul Sengupta <rahul.sengupta@match.com>
2020-07-02 12:22:45 -0500

    adds main activity

---
+++ app/src/main/java/com/rahulsengupta/livepaper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rahulsengupta.livepaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
